e7aa7b86c074c0d733086d43d1f09bb04c55b1b9
Alexman507 <Nerocross507@gmail.com>
2022-08-17 00:10:38 +0300

    views

--- project/models.py
+++ project/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 from project.setup.db import models
 
@@ -7,3 +8,35 @@ class Genre(models.Base):
     __tablename__ = 'genres'
 
     name = Column(String(100), unique=True, nullable=False)
+
+
+class Director(models.Base):
+    __tablename__ = 'directors'
+
+    name = Column(String(100), unique=True, nullable=False)
+
+
+class Movie(models.Base):
+    __tablename__ = 'movies'
+
+    title = Column(String(100), unique=True, nullable=False)
+    description = Column(String(100), unique=True, nullable=False)
+    trailer = Column(String(100), unique=True, nullable=False)
+    year = Column(Integer)
+    rating = Column(Float)
+    genre_id = Column(ForeignKey(f"{Genre.__tablename__}.id"), unique=True, nullable=False)
+    director_id = Column(ForeignKey(f"{Director.__tablename__}.id"), nullable=False)
+    genre = relationship("Genre")
+    director = relationship("Director")
+
+
+class User(models.Base):
+    __tablename__ = 'users'
+
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(300), unique=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    surname = Column(String(100), nullable=False)
+    favorite_genre = Column(Integer, ForeignKey(f"{Genre.__tablename__}.id"))
+
+
---
+++ project/views/main/movies.py
@@ -0,0 +1,29 @@
+from flask_restx import Namespace, Resource
+
+from project.container import movie_service
+from project.setup.api.models import movie
+from project.setup.api.parsers import page_parser
+
+api = Namespace('movies')
+
+
+@api.route('/')
+class MoviesView(Resource):
+    @api.expect(page_parser)
+    @api.marshal_with(movie, as_list=True, code=200, description='OK')
+    def get(self):
+        """
+        Get all movies.
+        """
+        return movie_service.get_all(**page_parser.parse_args())
+
+
+@api.route('/<int:genre_id>/')
+class MovieView(Resource):
+    @api.response(404, 'Not Found')
+    @api.marshal_with(movie, code=200, description='OK')
+    def get(self, movie_id: int):
+        """
+        Get movie by id.
+        """
+        return movie_service.get_item(movie_id)
